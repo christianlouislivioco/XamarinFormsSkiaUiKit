b2d60718b939c67a351270f87041bd6f4e0140cb
Christian Louis Livioco <christianlouislivioco@gmail.com>
2019-05-19 18:43:37 +0800

    Commit

---
+++ SkiaUiKit/SkiaUiKit/CustomControls/Checkbox.cs
@@ -0,0 +1,141 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Label = Xamarin.Forms.Label;
+
+namespace SkiaUiKit.CustomControls
+{
+    public class Checkbox : StackLayout
+    {
+        private double _colorAlphaPercentage;
+        private double _currentRadius;
+        private SKCanvasView _ripple;
+        private TaskCompletionSource<object> _releasedTask = new TaskCompletionSource<object>();
+        private FontIcon _fontIcon;
+
+        public Checkbox()
+        {
+            this.Spacing = 0;
+            this.Orientation = StackOrientation.Horizontal;
+
+            var tap = new TapGestureRecognizer();
+            this.GestureRecognizers.Add(tap);
+
+            tap.Tapped += Tap_Tapped;
+
+            _ripple = new SKCanvasView()
+            {
+                EnableTouchEvents = true
+            };
+
+            _ripple.PaintSurface += Ripple_PaintSurface;
+            _ripple.Touch += Ripple_Touch;
+
+            var grid = new Grid()
+            {
+                WidthRequest = 48,
+                HeightRequest = 48,
+            };
+
+            _fontIcon = new FontIcon() { VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center, Glyph = "\uE846" };
+            grid.Children.Add(_fontIcon);
+            grid.Children.Add(_ripple);
+
+            this.Children.Add(grid);
+            this.Children.Add(new Label() { Text = "Check me!", VerticalOptions = LayoutOptions.Center });
+        }
+
+        private void Tap_Tapped(object sender, System.EventArgs e)
+        {
+            if (IsChecked == true)
+            {
+                IsChecked = false;
+                _fontIcon.Glyph = "\uE846";
+            }
+            else
+            {
+                IsChecked = true;
+                _fontIcon.Glyph = "\uE845";
+            }
+        }
+
+        private void Ripple_Touch(object sender, SKTouchEventArgs e)
+        {
+            switch (e.ActionType)
+            {
+                case SKTouchAction.Pressed:
+                    var area = 24;
+
+                    (this).Animate("ScaleAnim",
+                                   new Animation((s) => RippleAction(s, area), _currentRadius = (float)(area * 0.08), area),
+                                   16,
+                                   length: 250,
+                                   easing: Easing.SinOut,
+                                   finished: async (d, b) => await FadeAsync());
+                    break;
+                case SKTouchAction.Released:
+                    if (_releasedTask.Task.Status != TaskStatus.RanToCompletion)
+                    {
+                        _releasedTask.SetResult(null);
+                    }
+                    break;
+            }
+
+            e.Handled = true;
+        }
+
+        private async Task FadeAsync()
+        {
+            await _releasedTask.Task;
+            _releasedTask = new TaskCompletionSource<object>();
+
+            (this).Animate("FadeAnim",
+                           new Animation((s) => FadeAction(s), _colorAlphaPercentage, 0),
+                           16,
+                           250,
+                           Easing.SinOut);
+        }
+
+        private void RippleAction(double s, int area)
+        {
+            _colorAlphaPercentage = _currentRadius / area;
+            _ripple.InvalidateSurface();
+            _currentRadius = (float)s;
+        }
+
+        private void FadeAction(double s)
+        {
+            _colorAlphaPercentage = s;
+            _ripple.InvalidateSurface();
+        }
+
+        private void Ripple_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+            canvas.Clear();
+
+            var paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.FromHex("#0060ac").MultiplyAlpha(0.32).MultiplyAlpha(_colorAlphaPercentage).ToSKColor(),
+                IsStroke = false,
+                IsAntialias = true,
+                IsDither = true
+            };
+
+            canvas.DrawCircle(24, 24, (float)_currentRadius, paint);
+        }
+
+        public static readonly BindableProperty IsCheckedProperty
+          = BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(FontIcon), default(bool));
+
+        public bool IsChecked
+        {
+            get => (bool)GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+    }
+}
---
+++ SkiaUiKit/SkiaUiKit/CustomControls/FontIcon.cs
@@ -0,0 +1,93 @@
+﻿using System.Runtime.CompilerServices;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace SkiaUiKit.CustomControls
+{
+    public class FontIcon : ContentView
+    {
+        private const double DefaultFontIconSize = 24;
+        private SKCanvasView _canvasView = new SKCanvasView();
+
+        public FontIcon()
+        {
+            this.WidthRequest = DefaultFontIconSize;
+            this.HeightRequest = DefaultFontIconSize;
+
+            _canvasView.PaintSurface += Canvas_PaintSurface;
+            this.Content = _canvasView;
+        }
+
+        private void Canvas_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var info = e.Info;
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            using (var textPaint = new SKPaint())
+            {
+                textPaint.IsAntialias = true;
+                textPaint.Color = this.Color.ToSKColor();
+                textPaint.Typeface = SKTypeface.FromFile(this.Typeface);
+
+                var textWidth = textPaint.MeasureText(this.Glyph);
+                textPaint.TextSize = this.GlyphSize;
+
+                var textBounds = new SKRect();
+                textPaint.MeasureText(this.Glyph, ref textBounds);
+
+                float xText = info.Width / 2 - textBounds.MidX;
+                float yText = info.Height / 2 - textBounds.MidY;
+
+                canvas.DrawText(this.Glyph, xText, yText, textPaint);
+            }
+        }
+
+        public static readonly BindableProperty GlyphProperty
+            = BindableProperty.Create(nameof(Glyph), typeof(string), typeof(FontIcon), default(string));
+
+        public string Glyph
+        {
+            get => (string)GetValue(GlyphProperty);
+            set => SetValue(GlyphProperty, value);
+        }
+
+        public static readonly BindableProperty TypefaceProperty
+            = BindableProperty.Create(nameof(Typeface), typeof(string), typeof(FontIcon), "Assets/fontello.ttf");
+
+        public string Typeface
+        {
+            get => (string)GetValue(TypefaceProperty);
+            set => SetValue(TypefaceProperty, value);
+        }
+
+        public static readonly BindableProperty ColorProperty
+            = BindableProperty.Create(nameof(Color), typeof(Color), typeof(FontIcon), Color.Black);
+
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        public static readonly BindableProperty GlyphSizeProperty 
+            = BindableProperty.Create(nameof(GlyphSize), typeof(float), typeof(FontIcon), (float)DefaultFontIconSize);
+
+        public float GlyphSize
+        {
+            get => (float)GetValue(GlyphSizeProperty);
+            set => SetValue(GlyphSizeProperty, value);
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == nameof(this.Glyph))
+            {
+                _canvasView.InvalidateSurface();
+            }
+        }
+    }
+}
